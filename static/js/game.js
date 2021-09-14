f907b626bbc179556cb8c11493fdc9e855ecd728
egor_masterskikh <masterskikh.egor@gmail.com>
2021-09-14 09:12:03 +0300

    add a drag and drop implementation in pure JS
    Greensock does not change the DOM, but uses translate3d

--- static/js/game.js
+++ static/js/game.js
@@ -1,29 +1,15 @@
-Draggable.create(".figure", {
-    bounds: ".board",
-    zIndexBoost: false,
-    onDragStart: function (e) {
-        // TODO: разместить фигуру так, чтобы курсор оказался в её центре
-    },
-    onDragEnd: function () {
-        let figure = $(this.target)
-        let figureSize = figure.width() // or figure.height()
+function dragstartHandler(event) {
+    event.dataTransfer.setData("text", event.target.parentElement.id)
+}
 
-        let figureLeft = figure.position().left
-        let figureTop = figure.position().top
+function dragoverHandler(event) {
+    event.preventDefault()
+    event.dataTransfer.dropEffect = "move"
+}
 
-        let figureLeft1 = Math.round(figureLeft / figureSize) * figureSize
-        let figureTop1 = Math.round(figureTop / figureSize) * figureSize
-
-        let figureXOffset = figureLeft1 - figureLeft
-        let figureYOffset = figureTop1 - figureTop
-
-        let [, translateX, translateY] = this.target.style.transform.match(/^translate3d\((-?\d+)px, (-?\d+)px, 0px\)$/)
-        translateX = Number(translateX)
-        translateY = Number(translateY)
-
-        let translateX1 = translateX + figureXOffset
-        let translateY1 = translateY + figureYOffset
-
-        figure.css("transform", `translate3d(${translateX1}px, ${translateY1}px, 0px)`)
-    }
-})
+function dropHandler(event) {
+    event.preventDefault()
+    const squareId = event.dataTransfer.getData("text")
+    const figure = document.getElementById(squareId).firstElementChild
+    event.target.appendChild(figure)
+}
