0e0558a9fb1735117b01711a1c14712848dad041
egor_masterskikh <masterskikh.egor@gmail.com>
2021-09-20 11:00:11 +0300

    implement drag and drop via mouse events
    built-in drag and drop events have limitations

--- static/js/game.js
+++ static/js/game.js
@@ -1,15 +1,46 @@
-function dragstartHandler(event) {
-    event.dataTransfer.setData("text", event.target.parentElement.id)
-}
+const figures = document.querySelectorAll(".figure")
 
-function dragoverHandler(event) {
-    event.preventDefault()
-    event.dataTransfer.dropEffect = "move"
-}
+figures.forEach(f => {
+    f.onmousedown = function (event) {
+        if (event.button !== 0) {
+            return
+        }
 
-function dropHandler(event) {
-    event.preventDefault()
-    const squareId = event.dataTransfer.getData("text")
-    const figure = document.getElementById(squareId).firstElementChild
-    event.target.appendChild(figure)
-}
+        function move(event) {
+            f.style.left = event.x - size / 2 + "px"
+            f.style.top = event.y - size / 2 + "px"
+        }
+
+        const size = f.getBoundingClientRect().width
+
+        f.style.width = size + "px"
+        f.style.height = size + "px"
+        f.style.position = "absolute"
+        f.style.cursor = "grabbing"
+
+        document.body.appendChild(f)
+
+        move(event)
+
+        document.onmousemove = function (event) {
+            move(event)
+        }
+
+        f.onmouseup = function (event) {
+            document.onmousemove = null
+
+            const square = document.elementsFromPoint(event.x, event.y).find(sq => {
+                return sq.classList.contains("square")
+            })
+
+            square.appendChild(f)
+
+            f.style.position = null
+            f.style.width = null
+            f.style.height = null
+            f.style.cursor = null
+            f.style.left = null
+            f.style.top = null
+        }
+    }
+})
