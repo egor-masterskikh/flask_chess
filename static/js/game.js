20ac14d66b80f0c6fc123064827bf6368ed1aaf4
egor_masterskikh <masterskikh.egor@gmail.com>
2021-09-20 21:43:21 +0300

    remove the relative positioning for the board, add the id to the figures and organize the js code blocks as functions

--- static/js/game.js
+++ static/js/game.js
@@ -2,26 +2,45 @@ const figures = document.querySelectorAll(".figure")
 
 figures.forEach(f => {
     f.onmousedown = function (event) {
+        // только левая кнопка мыши
         if (event.button !== 0) {
             return
         }
+
+        const size = f.getBoundingClientRect().width
 
         function move(event) {
             f.style.left = event.x - size / 2 + "px"
             f.style.top = event.y - size / 2 + "px"
         }
 
-        const size = f.getBoundingClientRect().width
+        function setStyle() {
+            f.style.width = size + "px"
+            f.style.height = size + "px"
+            f.style.position = "absolute"
+            f.style.cursor = "grabbing"
+        }
 
-        f.style.width = size + "px"
-        f.style.height = size + "px"
-        f.style.position = "absolute"
-        f.style.cursor = "grabbing"
+        function clearStyle() {
+            f.style.position = null
+            f.style.width = null
+            f.style.height = null
+            f.style.cursor = null
+            f.style.left = null
+            f.style.top = null
+        }
 
-        document.body.appendChild(f)
+        function getSquareFromPoint(event) {
+            return document.elementsFromPoint(event.x, event.y).find(sq => {
+                return sq.classList.contains("square")
+            })
+        }
 
+        setStyle()
         move(event)
 
+        /* при быстром перемещении курсор может оказаться вне фигуры,
+         поэтому onmousemove применяем к документу */
         document.onmousemove = function (event) {
             move(event)
         }
@@ -29,18 +48,11 @@ figures.forEach(f => {
         f.onmouseup = function (event) {
             document.onmousemove = null
 
-            const square = document.elementsFromPoint(event.x, event.y).find(sq => {
-                return sq.classList.contains("square")
-            })
+            const square = getSquareFromPoint(event)
 
             square.appendChild(f)
 
-            f.style.position = null
-            f.style.width = null
-            f.style.height = null
-            f.style.cursor = null
-            f.style.left = null
-            f.style.top = null
+            clearStyle()
         }
     }
 })
